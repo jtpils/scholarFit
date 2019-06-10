982cd29d4031e85a09c04afceb0b89d8c57cb89a
Farhad Maleki <farhad.maleki@usask.ca>
2019-06-10 00:10:24 -0400

    Implement TFIDFVectorizer

--- test/test_transformer.py
+++ test/test_transformer.py
@@ -25,3 +25,32 @@ class TestLabelEncoder(unittest.TestCase):
         new_encoder = LabelEncoder.load(address)
         self.assertListEqual(list(encoder.transform(labels)),
                              list(new_encoder.transform(labels)))
+
+class TestTFIDFVectorizer(unittest.TestCase):
+    def setUp(self):
+        self.vectorizer = TFIDFVectorizer()
+        self.corpus = ['This is the first document.',
+                       'This document is the second document.',
+                       'And this is the third one.',
+                       'Is this the first document?']
+        self.vectorizer.fit(self.corpus)
+
+    def test_fit(self):
+        feature_names = ['and', 'document', 'first', 'is', 'one', 'second',
+                         'the', 'third', 'this']
+        self.assertListEqual(list(self.vectorizer.get_feature_names()),
+                             feature_names)
+
+    def test_transform(self):
+        X = self.vectorizer.transform(self.corpus)
+        self.assertTupleEqual((X.shape), (4, 9))
+
+    def test_load_save(self):
+        address = 'temp/vectorizer.cod'
+        self.vectorizer.save(address)
+        new_vectorizer = TFIDFVectorizer.load(address)
+        self.assertListEqual(list(self.vectorizer.get_feature_names()),
+                             list(new_vectorizer.get_feature_names()))
+        X1 = self.vectorizer.transform(self.corpus)
+        X2 = new_vectorizer.transform(self.corpus)
+        self.assertAlmostEqual(LA.norm(X1.todense()), LA.norm(X2.todense()))
--- transformer.py
+++ transformer.py
@@ -85,3 +85,69 @@ class LabelEncoder(Transformer):
         with open(address, 'rb') as fin:
             return pickle.load(fin)
 
+
+class TFIDFVectorizer(Transformer):
+    '''Convert a collection of raw documents to a matrix of TF-IDF features.
+
+    This class serves as a wrapper for TfidfVectorizer object from
+        sklearn.feature_extraction.text.
+    '''
+    def __init__(self, **kwargs):
+        self.transformer = TfidfVectorizer(**kwargs)
+
+    def fit(self, corpus):
+        '''Learn vocabulary and idf from training set.
+
+        A wrapper for fit method of sklearn.preprocessing.LabelEncoder.
+            See the sklearn.feature_extraction.text.TfidfVectorizer.fit
+            method for more information.
+
+        '''
+        self.transformer.fit(corpus)
+
+    def transform(self, text):
+        '''Transform documents to document-term matrix.
+
+        A wrapper for transform method of TfidfVectorizer from
+            sklearn.feature_extraction.text. See transform method from
+            sklearn.feature_extraction.text import TfidfVectorizer.
+
+        '''
+        return self.transformer.transform(text)
+
+    def inverse_transform(self, encoded_text):
+        '''Return terms per document with nonzero entries in X.
+
+        A wrapper for inverse_transform method of TfidfVectorizer. See
+            sklearn.feature_extraction.text.TfidfVectorizer.inverse_transform
+            method for more information.
+
+        '''
+        return self.transformer.inverse_transform(encoded_text)
+
+    def get_feature_names(self):
+        '''Array mapping from feature integer indices to feature name.
+
+        Returns:
+            An array of feature names
+        '''
+        return self.transformer.get_feature_names()
+
+    def save(self, address):
+        '''Write a TfidfVectorizer object to file.
+
+        Args:
+            address: Address of the file that TFIDFVectorizer will be written into.
+        '''
+        with open(address, 'wb') as fout:
+            pickle.dump(self.transformer, fout)
+
+    @classmethod
+    def load(cls, address):
+        '''Load a TfidfVectorizer object from file.
+
+        Args:
+            address: Address of the file that TFIDFVectorizer will be read from.
+        '''
+        with open(address, 'rb') as fin:
+            return pickle.load(fin)
